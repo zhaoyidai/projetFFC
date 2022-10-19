302195f02037169ab0a95f2344479e6592e37e33
HDiana <ghieme.moungoua@gmail.com>
2022-10-19 09:51:10 +0200

    Méthodes HashCode et Equals définit pour un Sprint et un Col

--- src/model/Edition.java
+++ src/model/Edition.java
@@ -5,6 +5,8 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
 
 /**
  *
@@ -18,16 +20,16 @@ public class Edition {
     protected Coureur meilleurGrimpeur;
     protected Coureur meilleurJeune;
     protected Course course;
-    protected ArrayList<InscriptionEquipe> equipes;
-    protected ArrayList<InscriptionCoureur> coureurs;
+    protected Hashtable<InscriptionEquipe, ClassementGE> classementEquipes;
+    protected Hashtable<InscriptionCoureur, ClassementGC> classementCoureurs;
     
     //Constructeur
     public Edition(String dateDebutEdition, String dateFinEdition,Course course){
         this.dateDebutEdition=dateDebutEdition;
         this.dateFinEdition=dateFinEdition;
         this.course=course;
-        this.coureurs= new ArrayList<InscriptionCoureur>();
-        this.equipes= new ArrayList<InscriptionEquipe>();
+        this.classementCoureurs = new Hashtable<>();
+        this.classementEquipes= new Hashtable<>();
         this.course.editions.add(this);
     }
     
@@ -56,18 +58,17 @@ public class Edition {
     
     //Inscrit une équipe à l'édition de course
     public void inscrireEquipe(InscriptionEquipe equipe){
-        this.equipes.add(equipe);
+        this.classementEquipes.put(equipe,null);
     }
     
     //Inscrit un coureur à l'édition de course
     public void inscrireCoureur(InscriptionCoureur coureur){
-        this.coureurs.add(coureur);
+        this.classementCoureurs.put(coureur, null);
     }
     
     //Retourne la liste des coureurs de l'édition de course
-    public ArrayList<InscriptionCoureur> obtenirlisteCoureurs(){
-        
-        return this.coureurs;
+    public Hashtable obtenirlisteCoureurs(){
+        return this.classementCoureurs;
         
     }
     
@@ -98,9 +99,9 @@ public class Edition {
     }
     
     //Retourne la liste des équipes qui participent à l'édition de course
-    public ArrayList<InscriptionEquipe> obtenirListeEquipes(){
+    public Hashtable obtenirListeEquipes(){
            
-        return this.equipes;
+        return this.classementEquipes;
            
     }
     
@@ -121,6 +122,4 @@ public class Edition {
         
     }
     
-    
-
 }
