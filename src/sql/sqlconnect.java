29d90ba8e4ad46b0ba07a62ffe267b037081b0dd
zdai2 <zdai2923@gmail.com>
2022-10-21 13:06:15 +0200

    main pour presenter 1

--- src/sql/sqlconnect.java
+++ src/sql/sqlconnect.java
@@ -4,24 +4,42 @@
  */
 package sql;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author utiae
  */
 public class sqlconnect {
-    Connection con = null;
-    
+
+    private Connection con;
+	private PreparedStatement ghysql;
+	private ResultSet res;  //返回的Result
+	
+	public static void main(String[] args){
+		sqlconnect sql=new sqlconnect();
+		sql.getConnection();
+	}
     public static Connection getConnection(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ffc","root", "root"); 
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ffc","root", "3377"); 
             return con;
         } catch( Exception e ) {
             return null;
         }
     }
     
-    public void sqlclose() throws SQLException {
-        con.close();
-    }
+    public void sqlclose(){
+		//在处理完结果集的时候再关闭连接，关掉连接后处理结果集会发生此错误。
+		if(ghysql !=null){
+			try{
+				ghysql.close();
+				System.out.println("释放并关闭了sql空间");
+			}catch(SQLException e){
+				System.err.println(e.getMessage());
+			}
+			ghysql=null;
+		}
+	}
 }
